b0b2493fbed99fefef6e0f704a11bdf3d8448143
Ivan Putra Eriansya <eriansha.van@gmail.com>
2024-07-27 21:13:47 +0700

    feat: create user

--- main.go
+++ main.go
@@ -6,22 +6,54 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 const (
 	dbDriver = "mysql"
-	dbUser = "root"
-	dbPass = "root"
-	dbName = "gocrud_app"
+	dbUser   = "root"
+	dbPass   = "root"
+	dbName   = "gocrud_app"
 )
 
 type User struct {
 	Id    int
 	Name  string
 	Email string
+}
+
+func createUser(db *sql.DB, name, email string) error {
+	query := "INSERT INTO users(name, email) values (?, ?)"
+
+	_, err := db.Exec(query, name, email)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	CreateUser(db, user.Name, user.Email)
+	if err != nil {
+		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprint(w, "User created successfully")
 }
 
 func main() {
